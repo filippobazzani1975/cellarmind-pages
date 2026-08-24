30222365a29cef5131d66f0fd0ad88caaebdb95b
Filippo Bazzani <310920646+filippobazzani1975@users.noreply.github.com>
2026-08-24 14:02:36 +0200

    fix: separate Android document and image pickers

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'cellarmind-app-shell-'
-const CACHE_NAME = `${CACHE_PREFIX}v3`
+const CACHE_NAME = `${CACHE_PREFIX}v4`
 const APP_SHELL = ['./manifest.webmanifest', './favicon.svg']
 
 const isCacheableResponse = (response) =>
