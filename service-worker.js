2e9fc8e8e1fd4617e3bf2d4dd9f4e9fcca3d2c52
Filippo Bazzani <310920646+filippobazzani1975@users.noreply.github.com>
2026-08-24 13:55:36 +0200

    fix: open Android document picker for imports

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'cellarmind-app-shell-'
-const CACHE_NAME = `${CACHE_PREFIX}v2`
+const CACHE_NAME = `${CACHE_PREFIX}v3`
 const APP_SHELL = ['./manifest.webmanifest', './favicon.svg']
 
 const isCacheableResponse = (response) =>
