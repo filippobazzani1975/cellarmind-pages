27b84a56c5c2dd1e369c2e988fb9b03d2a0c9874
Filippo Bazzani <310920646+filippobazzani1975@users.noreply.github.com>
2026-08-24 12:50:09 +0200

    deploy: publish CellarMind smartphone PWA

---
+++ service-worker.js
@@ -0,0 +1,101 @@
+const CACHE_PREFIX = 'cellarmind-app-shell-'
+const CACHE_NAME = `${CACHE_PREFIX}v1`
+const APP_SHELL = ['./manifest.webmanifest', './favicon.svg']
+
+const isCacheableResponse = (response) =>
+  response.ok && response.type !== 'opaque'
+
+const cacheBuiltApplication = async () => {
+  const cache = await caches.open(CACHE_NAME)
+  const indexUrl = new URL('./index.html', self.registration.scope)
+  const indexResponse = await fetch(indexUrl, { cache: 'reload' })
+
+  if (!isCacheableResponse(indexResponse)) {
+    throw new Error('Application shell unavailable')
+  }
+
+  const html = await indexResponse.clone().text()
+  const assetUrls = Array.from(
+    html.matchAll(/(?:src|href)="([^"]+)"/gu),
+    (match) => new URL(match[1], self.registration.scope),
+  ).filter((url) => url.origin === self.location.origin)
+
+  await cache.addAll([...APP_SHELL, ...assetUrls])
+  await cache.put('./index.html', indexResponse.clone())
+  await cache.put('./', indexResponse)
+}
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    cacheBuiltApplication().then(() => self.skipWaiting()),
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((names) =>
+        Promise.all(
+          names
+            .filter(
+              (name) => name.startsWith(CACHE_PREFIX) && name !== CACHE_NAME,
+            )
+            .map((name) => caches.delete(name)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request
+
+  if (request.method !== 'GET') {
+    return
+  }
+
+  const url = new URL(request.url)
+
+  if (url.origin !== self.location.origin) {
+    return
+  }
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          if (isCacheableResponse(response)) {
+            const copy = response.clone()
+            void caches.open(CACHE_NAME).then((cache) => cache.put('./', copy))
+          }
+          return response
+        })
+        .catch(async () => {
+          const cache = await caches.open(CACHE_NAME)
+          return (
+            (await cache.match('./')) ??
+            (await cache.match('./index.html')) ??
+            Response.error()
+          )
+        }),
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached !== undefined) {
+        return cached
+      }
+
+      return fetch(request).then((response) => {
+        if (isCacheableResponse(response)) {
+          const copy = response.clone()
+          void caches.open(CACHE_NAME).then((cache) => cache.put(request, copy))
+        }
+        return response
+      })
+    }),
+  )
+})
